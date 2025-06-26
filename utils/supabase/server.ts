21d9d22f01785a838140e12b6fb89656d1e0a3ab
Fernando Jose Guevara Palma <gp24006@ues.edu.sv>
2025-06-26 10:54:33 -0600

    ya no se
    sigo con el error

--- utils/supabase/server.ts
+++ utils/supabase/server.ts
@@ -2,30 +2,28 @@ import { createServerClient, type CookieOptions } from '@supabase/ssr'
 import { cookies } from 'next/headers'
 
 export function createClient() {
+  const cookieStore = cookies()
+
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       cookies: {
-        // Hacemos que cada método sea ASÍNCRONO para poder usar 'await'
-        async get(name: string) {
-          // ESPERAMOS a que la promesa de las cookies se resuelva
-          return (await cookies().get(name)?.value)
+        get(name: string) {
+          return cookieStore.get(name)?.value
         },
-        async set(name: string, value: string, options: CookieOptions) {
+        set(name: string, value: string, options: CookieOptions) {
           try {
-            // ESPERAMOS a que la promesa de las cookies se resuelva
-            await cookies().set({ name, value, ...options })
+            cookieStore.set({ name, value, ...options })
           } catch (error) {
             // The `set` method was called from a Server Component.
             // This can be ignored if you have middleware refreshing
             // user sessions.
           }
         },
-        async remove(name: string, options: CookieOptions) {
+        remove(name: string, options: CookieOptions) {
           try {
-            // ESPERAMOS a que la promesa de las cookies se resuelva
-            await cookies().set({ name, value: '', ...options })
+            cookieStore.set({ name, value: '', ...options })
           } catch (error) {
             // The `delete` method was called from a Server Component.
             // This can be ignored if you have middleware refreshing
