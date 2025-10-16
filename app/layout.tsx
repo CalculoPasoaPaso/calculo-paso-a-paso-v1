a45cf2038378d99e2c2e6b4b75ecaa5987303788
Fernando Jose Guevara Palma <gp24006@ues.edu.sv>
2025-10-15 21:22:43 -0600

    no funcionaba el boton

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter, Noto_Sans, Public_Sans } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "./components/theme-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 const noto_sans = Noto_Sans({ subsets: ["latin"], weight: ['400', '500', '700', '900'], variable: '--font-noto-sans' });
@@ -19,8 +20,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="es"> {/* Cambiado a español */}
-      <body className={`${inter.className} ${noto_sans.variable} ${public_sans.variable}`}>{children}</body>
+    <html lang="es" suppressHydrationWarning> {/* Cambiado a español y añadido suppressHydrationWarning */}
+      <body className={`${inter.className} ${noto_sans.variable} ${public_sans.variable}`}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
